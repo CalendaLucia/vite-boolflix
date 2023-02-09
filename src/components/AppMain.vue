c252b49488cf85498523786332dca186f1d40eed
CalendaLucia <calendalucia4@gmail.com>
2023-02-09 11:43:11 +0100

    mileston 1 completata

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,12 +11,15 @@ export default {
 </script>
 
 <template>
-    <div>
-        <ul>
-             <li class="bg-light" v-for="film in store.films">
-                   {{ film.title }}
-              </li>
-          </ul>
+    <div class="continer-fluid">
+      <div class="card">
+        <div  v-for="film in store.films" :key="film.id">
+           <h3>{{ film.title}}</h3>
+           <h4>{{ film.original_title}}</h4>
+           <h5>{{ film.original_language}}</h5>
+           <h6>{{ film.vote_average}}</h6>
+        </div>
+      </div>
     </div>
 </template>
 
