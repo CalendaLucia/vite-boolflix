f6a8022f50393feee953564327b1f37c08a3a01b
CalendaLucia <calendalucia4@gmail.com>
2023-02-09 13:06:10 +0100

    fine milestone 3

--- src/App.vue
+++ src/App.vue
@@ -67,7 +67,6 @@ export default {
 
 <template>
   <AppHeader @search="event"/>
-
   <AppMain/>
   <AppCard/>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -25,20 +25,34 @@ export default {
             }
             const flag = `https://www.worldometers.info//img/flags/small/tn_${lang}-flag.gif`;
             return flag;
-        }
-    }
+        },
+        generateImageURL(posterPath, size = 'w342') {
+           return `https://image.tmdb.org/t/p/${size}/${posterPath}`;
+        },
+        calculateStars(vote) {
+          return Math.ceil(vote / 2);
+        },
 }
+    }
+
 </script>
 
 <template>
     <div class="continer-fluid">
       <div class="card">
-        <div  v-for="film in store.films" :key="film.id">
+        
+        <div v-for="film in store.films" :key="film.id">
+           <img :src="generateImageURL(film.poster_path)">
            <h3>{{ film.title}}</h3>
            <h4>{{ film.original_title}}</h4>
            <img :src="flag(film.original_language)">
            <h5>{{ film.original_language}}</h5>
-           <h6>{{ film.vote_average}}</h6>
+           <div v-for="i in calculateStars(film.vote_average)" :key="i">
+              <i class="bi bi-star-fill"></i>
+           </div>
+           <div v-for="i in (5 - calculateStars(film.vote_average))" :key="i">
+             <i class="bi bi-star"></i>
+           </div>
         </div>
       </div>
     </div>
